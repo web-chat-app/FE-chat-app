652a5c7ed3eb87f2d629fa9c6f7e26365ac630e7
Saeed beraia <saeed.beraia1996@gmail.com>
2023-04-20 17:12:46 +0300

    edit socket url

--- src/socket.ts
+++ src/socket.ts
@@ -1,10 +1,7 @@
 import { io } from "socket.io-client";
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = ["production", "dev"].includes(import.meta.env.MODE ?? "")
-  ? undefined
-  : import.meta.env.VITE_SOCKET_URL;
+const URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3001";
 
-export const socket = io(URL ?? "", {
+export const socket = io(URL, {
   transports: ["websocket"],
 });
